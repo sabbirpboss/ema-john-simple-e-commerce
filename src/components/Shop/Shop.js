fb5fe748e806cb990316daf38f945a6f43607879
sabbirpboss <sabbir.ah799@gmail.com>
2022-03-25 16:41:41 +0600

    shopping cart done

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -4,6 +4,8 @@ import './Shop.css'
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([]);
+
     useEffect( () => {
         fetch('products.json')
         .then(res => res.json())
@@ -12,6 +14,9 @@ const Shop = () => {
 
     const handleAddToCart = (product) => {
         console.log(product);
+        // cart.push(product);
+        const newCart = [...cart, product];
+        setCart(newCart);
     }
 
     return (
@@ -27,6 +32,7 @@ const Shop = () => {
             </div>
             <div className="cart-container">
                 <h4>order cart</h4>
+                <p>Selected Items: {cart.length}</p>
             </div>
         </div>
     );
