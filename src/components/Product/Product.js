59064b3ce752a90e92214099c3d33a04ac558b2b
sabbirpboss <sabbir.ah799@gmail.com>
2022-03-25 16:10:27 +0600

    props shortcut method

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,7 +2,8 @@ import React from 'react';
 import './Product.css'
 
 const Product = (props) => {
-    const {img, name, price, seller, ratings} = props.product;
+    const {product, handleAddToCart} = props;
+    const {img, name, price, seller, ratings} = product;
     return (
         <div className='product'>
             <img src={img} alt="" />
@@ -10,7 +11,7 @@ const Product = (props) => {
             <p className='product-price'>Price: {price}</p>
             <p className='product-seller'>Manufacturer: {seller}</p>
             <p className='product-ratings'>Ratings: {ratings} star</p>
-            <button onClick={ () => props.handleAddToCart(props.product)} className="btn-cart">
+            <button onClick={ () => handleAddToCart(product)} className="btn-cart">
                 Add to Cart
             </button>
         </div>
