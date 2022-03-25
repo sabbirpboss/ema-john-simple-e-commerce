92e810eea103620c53a9c2a058d2b485c71cb676
sabbirpboss <sabbir.ah799@gmail.com>
2022-03-25 19:12:29 +0600

    font awesome added for dollar sign

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,13 +1,41 @@
-import React from 'react';
-import './Cart.css'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDollarSign } from "@fortawesome/free-solid-svg-icons";
+import "./Cart.css";
 
-const Cart = ({cart}) => {
-    return (
-        <div className='cart'>
-            <h2>this is Cart</h2>
-            <p>Selected Items: {cart.length}</p>
-        </div>
-    );
+const Cart = ({ cart }) => {
+  // console.log(cart);
+
+  let total = 0;
+  let shipping = 0;
+  for (const product of cart) {
+    total = total + product.price;
+    shipping = shipping + product.shipping;
+  }
+  const tax = parseFloat((total * 0.1).toFixed(2));
+  const grandTotal = total + shipping + tax;
+  return (
+    <div className="cart">
+      <h2>Order Summary</h2>
+      <p>Selected Items: {cart.length}</p>
+      <p>
+        Total price: <FontAwesomeIcon icon={faDollarSign}></FontAwesomeIcon>
+        {total}
+      </p>
+      <p>
+        Total Shipping: <FontAwesomeIcon icon={faDollarSign}></FontAwesomeIcon>
+        {shipping}
+      </p>
+      <p>
+        Tax: <FontAwesomeIcon icon={faDollarSign}></FontAwesomeIcon>
+        {tax}
+      </p>
+      <h5>
+        Grand Total: <FontAwesomeIcon icon={faDollarSign}></FontAwesomeIcon>
+        {grandTotal.toFixed(2)}
+      </h5>
+    </div>
+  );
 };
 
 export default Cart;
