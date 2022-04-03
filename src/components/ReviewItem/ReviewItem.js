61c093caaa28e3e4df39eecbf435fc2e7a844e0b
sabbirpboss <sabbir.ah799@gmail.com>
2022-04-03 14:41:25 +0600

    cart area styled

--- src/components/ReviewItem/ReviewItem.js
+++ src/components/ReviewItem/ReviewItem.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import './ReviewItem.css';
 
 const ReviewItem = ({product}) => {
-    const {name, price, shipping, quantity} = product;
+    const {name, img, price, shipping, quantity} = product;
     return (
-        <div>
-            <h3>This is review item name: {name}</h3>
+        <div className='review-item'>
+            <div className='cart-img'>
+                <img src={img} alt="" />
+            </div>
+            <div className="review-item-details-container">
+                <div className="review-item-details">
+                    <p className="product-name">
+                        {name}
+                    </p>
+                    <p className='cart-product-price'>Price: <span className="orange-color">&{price}</span></p>
+                    <p className='shipping'><small>Shipping: {shipping}</small></p>
+                    <p className='quantity'><small>Quantity: {quantity}</small></p>
+                </div>
+                <div className="delete-container">
+                    <button>Delete</button>
+                </div>
+            </div>
         </div>
     );
 };
