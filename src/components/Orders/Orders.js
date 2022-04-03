0a36e32ccee053675bf921efe2fb596db793dc5b
sabbirpboss <sabbir.ah799@gmail.com>
2022-04-03 12:42:38 +0600

    use own code by creating hooks

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import useCart from '../../hooks/useCart';
+import useProducts from '../../hooks/useProducts';
 
 const Orders = () => {
+    const [products, setProducts] = useProducts();
+    const [cart, setCart] = useCart(products);
     return (
         <div>
-            <h2>This is orders</h2>
+            <h2>This is orders: {products.length}</h2>
+            <p>cart has: {cart.length}</p>
         </div>
     );
 };
