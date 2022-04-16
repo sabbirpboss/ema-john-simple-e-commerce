931d3ac592e2a736386ac52a8cf5b0e31f974157
sabbirpboss <sabbir.ah799@gmail.com>
2022-04-17 02:35:09 +0600

    steps

---
+++ src/__auth_steps.js
@@ -0,0 +1,19 @@
+/**
+ * 1. Create a new project in console.firebase.google.com
+ * 2. Add Firebase to your web app:i)set name, ii)host your site
+ * 3. npm install firebase
+ * 4. initialize firebase(copy text) and create a file under src named firebase.init.js and paste here firebase initialize text
+ * 5. import getAuth and export default app
+ * 6. go to console and then firebase settings > Authentication > enable needed auth providers
+ * 7. 
+*/
+
+
+
+/**
+ * hosting steps
+ * 1. npm install -g firebase-tools
+ * 2. firebase login
+ * 3. firebase init
+ * 4. firebae deploy
+*/
